b3b0506e9d12b62ff1934801dcd900738fb966f7
crcprod50 <crcprod50@gmail.com>
2024-11-08 07:08:36 +0000

    first checkin

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new Streamlit app -1 ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building the app! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
